979ae7eeb675290bd857f085a5be83deee0a965f
apitap <48079342+apitap@users.noreply.github.com>
2025-12-07 23:16:05 +0700

    Update github.sql

--- examples/sql/github.sql
+++ examples/sql/github.sql
@@ -1,12 +1,5 @@
 {{ sink(name="postgres_sink") }}
 
 SELECT 
-    t.content['id'] as id,
-    t.content['type'] as content_type,
-    t.content['status'] as content_status,
-    t.content['title'] as content_title,
-    t.content['restrictions'] as content_restrictions,
-    t.content['_links'] as content_links,
-    t.content['_links']['webui'] as content_webui,
-    t.content['_links']['self'] as content_self
-FROM {{ use_source("confluence_content") }} AS t;
+    *
+FROM {{ use_source("github_repos") }} AS t;
